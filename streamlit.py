74b028c99d3fad41ae73fe48f9c6283d48a862ec
alyatoon <149295614+alyatoon@users.noreply.github.com>
2023-10-29 19:31:09 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,33 +7,33 @@ st.header("", divider='rainbow')
 
 col1, col2, col3 = st.columns(3)
 with col1: 
-    Bp = st.number_input("Tekanan darah", 50,180,50)
+    Bp = st.number_input("Tekanan darah", 50,180,80)
 with col2:
-    Sg = st.number_input("Specific Gravity normal 1.0005 - 1.030", 1.0, 1.02)
+    Sg = st.number_input("Specific Gravity normal", 1.00, 1.030, 1.025, step=0.001, format="%0.3f")
 with col3:  
-    Al = st.number_input("Albumin", 0,5,1) 
+    Al = st.number_input("Albumin", 0,5,0) 
 
 with col1:
     Su = st.number_input("Gula",0,5,0) 
 with col2:
     Rbc = st.number_input("Sel darah merah", 0,1,1) 
 with col3:
-    Bu = st.number_input("Darah urea") 
+    Bu = st.number_input("Darah urea",1.5,391.1,10.0) 
 with col1:
-    Sc = st.number_input("Serum creatine",0.4,76.0,0.5) 
+    Sc = st.number_input("Serum creatine",0.4,76.0,1.2) 
 with col2:
-    Sod = st.number_input("Sodium",4.5,168.0,130.0) 
+    Sod = st.number_input("Sodium",4.5,168.0,135.0) 
 with col3:
-    Pot = st.number_input("Pottasium",2.5,47.0,3.2) 
+    Pot = st.number_input("Pottasium",2.5,47.0,5.0) 
 
 with col1:
-    Hemo = st.number_input("Hemoglobin",3.1,17.8,4.0) 
+    Hemo = st.number_input("Hemoglobin",3.1,17.8,15.0) 
 with col2:
-    Wbcc = st.number_input("Jumlah sel darah putih",2200,26400,3000) 
+    Wbcc = st.number_input("Jumlah sel darah putih",2200,26400,10400) 
 with col3:
-    Rbcc = st.number_input("Jumlah sel darah merah", 2.1, 8.0,5.0) 
+    Rbcc = st.number_input("Jumlah sel darah merah", 2.1, 8.0,4.5) 
     
-Htn = st.selectbox("Hipertensi",["Ya", "Tidak"])
+Htn = st.selectbox("Hipertensi",["Tidak", "Ya"])
 if Htn == "Ya" : 
     Htn = 1 
 else : 
